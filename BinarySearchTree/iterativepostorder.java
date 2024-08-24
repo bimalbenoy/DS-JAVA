36c47f46614d8a4054e1a5a08535af526127f54f
bimalbenoy <bimalbenoy0@gmail.com>
2024-08-24 20:25:48 +0530

    Create iterativepostorder.java

---
+++ BinarySearchTree/iterativepostorder.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        Stack<TreeNode> st =new Stack<>();
+        if(root!=null) st.push(root);
+        while(st.size()>0){
+            TreeNode x=st.pop();
+            ans.add(x.val);
+            if(x.left!=null) st.push(x.left);
+            if(x.right!=null) st.push(x.right);
+            
+        }
+        Collections.reverse(ans);
+        return ans;
+
+        
+    }
+}
