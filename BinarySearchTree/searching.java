678535f3a18a9f4fa0d722ec8274936f88274c6a
bimalbenoy <bimalbenoy0@gmail.com>
2024-09-12 07:40:58 +0530

    Create searching.java

---
+++ BinarySearchTree/searching.java
@@ -0,0 +1,7 @@
+ public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null) return null;
+        if(root.val<val) return searchBST(root.right,val );
+        else if(root.val>val) return searchBST(root.left,val);
+        else return root;
+
+    }
