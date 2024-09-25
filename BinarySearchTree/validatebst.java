b643af7de66614b6591b4fc10c826929e9e992b6
bimalbenoy <bimalbenoy0@gmail.com>
2024-09-25 23:27:25 +0530

    Update validatebst.java

--- BinarySearchTree/validatebst.java
+++ BinarySearchTree/validatebst.java
@@ -17,3 +17,42 @@ class Solution {
         
     }
 }
+//or
+class Solution {
+    static boolean flag;
+    public long max(TreeNode root){
+        if (root==null) return Long.MIN_VALUE;
+        long a=root.val;
+
+        long b=max(root.left);
+        if(a<=b) flag=false;
+        long c=max(root.right);
+        
+        
+        return Math.max(a,Math.max(b,c));
+
+
+    }
+        public long min(TreeNode root){
+        if (root==null) return Long.MAX_VALUE;
+        long a=root.val;
+
+        long b=min(root.left);
+        
+        long c=min(root.right);
+        if(c<=a) flag=false;
+        
+        
+        return Math.min(a,Math.min(b,c));
+
+
+    }
+    public boolean isValidBST(TreeNode root) {
+        flag=true;
+        min(root);
+        max(root);
+        
+        return flag;
+        
+    }
+}
